33ea5c532085aeadaf4e1ceb2d4b672f2bc7953c
uttombarmon <uttombarmondev@gmail.com>
2025-05-29 19:23:02 +0600

    First program in C

---
+++ corefundamentals/firstprogram.c
@@ -0,0 +1,7 @@
+#include<stdio.h> // header library files
+
+int main() // function declaration
+{
+  printf("Hello World!"); //showing Hello World;
+  return 0; // function return
+} //{} function body or block
