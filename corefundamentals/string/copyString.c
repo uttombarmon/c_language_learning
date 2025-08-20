3bb931644b13aa601fa437b66a45719272779d2d
uttombarmon <uttombarmondev@gmail.com>
2025-08-20 14:23:31 +0600

    added copyString.c

---
+++ corefundamentals/string/copyString.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  char str1[100], str2[100];
+  printf("Enter a string: ");
+  fgets(str1, sizeof(str1), stdin);
+  strcpy(str2, str1);
+  printf("First string: %s\n", str1);
+  printf("Second string: %s\n", str2);
+  return 0;
+}
